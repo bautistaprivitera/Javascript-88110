7c2100cddac9bc6569794860484a3b58fc375f63
bautistaprivitera <juanbautistaprivitera@gmail.com>
2025-05-29 12:08:15 -0300

    Clase 2

---
+++ Clase02/js/main.js
@@ -0,0 +1,23 @@
+/*Banco*/
+
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let usuario = prompt("Ingrese su nombre de usuario");
+let usuarioIngresado = prompt("Ingrese de nuevo su usuario para confirmarlo");
+let contraseña = prompt("Ingrese su contraseña");
+let contraseñaIngresada = prompt("Ingrese de nuevo su contraseña para confirmarla");
+let edad = prompt("Ingrese su edad")
+
+if(usuario === usuarioIngresado && contraseña === contraseñaIngresada && edad >= 18){
+
+    console.log("Ya creamos su cuenta bancaria, puede empezar a operar su dinero")
+}else{
+    console.log("No se ha podido crear debido a falta de informacion");
+}
+const metodoPago = "efectivo, tarjeta, cheque, otro metodo de pago";
+prompt("Ingrese su metodo de pago")
+switch(metodoPago){
+    case "efectivo":
+        console.log("Usted ha pagado con ${efectivo}")
+
+}
